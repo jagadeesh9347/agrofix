fd089c7c0458224027d6e8c8088c71f1e701fa8c
jagadeesh9347 <128576182+jagadeesh9347@users.noreply.github.com>
2025-04-20 15:08:26 +0530

    route.ts

--- src/app/api/admin/orders/[id]/route.ts
+++ src/app/api/admin/orders/[id]/route.ts
@@ -1,17 +1,19 @@
 import { prisma } from '@/lib/prisma';
-import { NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 
 export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
+  req: NextRequest,
+  context: { params: { id: string } }
 ) {
   try {
     const { status } = await req.json();
-    const orderId = parseInt(params.id, 10);
+    const orderId = parseInt(context.params.id, 10);
+
     await prisma.order.update({
       where: { id: orderId },
       data: { status },
     });
+
     return NextResponse.json({ success: true });
   } catch (error) {
     console.error('Error updating status:', error);
